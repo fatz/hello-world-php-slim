a949b98e0645187a41b2a8caf4f99c63fd86eeaf
Jan Ulferts <julferts@goodgamestudios.com>
2015-08-29 11:09:40 +0200

    added more awesomeness

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 $app->get('/', function () {
-    echo "<h1>Hello world v1</h1>";
+    echo "<h1>Hello world v2</h1>";
+    echo "<h2>Shipps more awesomeness</h2>";
+    echo "<h3>With an awful subtitle</h3>";
 });
 $app->get('/health', function () {
     echo "ALIVE";
