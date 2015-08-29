e92315b72e1a22d9c5f57c807b8a2ed8789032e8
Jan Ulferts <julferts@goodgamestudios.com>
2015-08-29 11:12:55 +0200

    fixed awful subtitle

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $app = new \Slim\Slim();
 $app->get('/', function () {
     echo "<h1>Hello world v2</h1>";
     echo "<h2>Shipps more awesomeness</h2>";
-    echo "<h3>With an awful subtitle</h3>";
+    echo "<h3>With an <em>awesome</em> subtitle</h3>";
 });
 $app->get('/health', function () {
     echo "ALIVE";
